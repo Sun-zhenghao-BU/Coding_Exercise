bd19dfe3d8008a3112469441533a7ade372b1064
Sun_zh <szh1007@bu.edu>
2023-07-14 23:59:10 -0400

    update

--- Sorting/src/QuickSort.java
+++ Sorting/src/QuickSort.java
@@ -1,6 +1,33 @@
 public class QuickSort {
     public static void main(String[] args) {
-        int i = -3 >> 1;
-        System.out.println(i);
+        check();
+    }
+
+    private static void check () {
+        for (int i = 6; i < 100000; i++) {
+            if (counterExample(i)) {
+                System.out.println("You have won Fields award!!!");
+            } else {
+                System.out.println(i + " is not a counter-example");
+            }
+        }
+    }
+
+    private static boolean counterExample(int i) {
+        for (int j = 2; j <= i / 2; j++) {
+            if (isPrime(j) && isPrime(j-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPrime(int num) {
+        for (int n = 2; n * n < num; n++) {
+            if (num % n == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
