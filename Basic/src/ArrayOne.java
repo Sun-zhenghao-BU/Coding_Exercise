ce1c35f15d704d48bc5b500870f50f193ce40667
Sun_zh <szh1007@bu.edu>
2023-07-23 21:59:37 -0400

    update

--- Basic/src/ArrayOne.java
+++ Basic/src/ArrayOne.java
@@ -8,6 +8,10 @@ public class ArrayOne {
         }
         int res = findMax(arr1);
         System.out.println(res);
+        int[] resArr = insertElement(arr1, 38, 7);
+        System.out.println(Arrays.toString(resArr));
+        reverseArray(resArr);
+        System.out.println(Arrays.toString(resArr));
     }
 
     public static int findMax(int[] arr) {
@@ -21,5 +25,34 @@ public class ArrayOne {
             }
         }
         return maxValue;
+    }
+
+    private static int[] insertElement(int[] arr, int ele, int pos) {
+            int len = arr.length;
+            int[] arr1 = new int[len + 1];
+            for (int i = 0, j = 0; i < arr1.length; i++, j++) {
+                arr1[i] = arr[j];
+                if (i == pos) {
+                    arr1[i] = ele;
+                    arr1[++i] = arr[j];
+                }
+            }
+            return arr1;
+    }
+
+    private static void reverseArray (int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
